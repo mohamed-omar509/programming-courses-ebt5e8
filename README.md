cc53d66f43f1534fc51ddd5f015cfff577700eb3
mohamed-omar509 <126252818+mohamed-omar509@users.noreply.github.com>
2023-02-25 13:02:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-ebt5e8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-ebt5e8)
---
+++ index.ts
@@ -0,0 +1,10 @@
+let Nnumber1, Nnumber2, Nnumber3: number;
+Number1 = parseInt(prompt('الرجاء ادخال الرقم الاول'));
+Nnumber2 = parseInt(prompt('الرجاء ادخال الرقم الثاني'));
+Nnumber3 = Number1 * Nnumber2;
+if (Nnumber3 > 0) {
+  console.log('الرقم موجب');
+} else {
+  console.log('الرقم سالب');
+}
+console.log('النهاية ');
